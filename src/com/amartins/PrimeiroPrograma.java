bb9ec4562297ea028e3e2adba6054ed3e1ce64fc
alaerciomartins <alaerciomartinsjr@gmail.com>
2022-05-06 13:49:35 -0300

    teste

--- src/com/amartins/PrimeiroPrograma.java
+++ src/com/amartins/PrimeiroPrograma.java
@@ -11,11 +11,8 @@ public class PrimeiroPrograma {
 		Livros livros = new Livros();
 		
 		System.out.println(gato);
-		System.out.println(livros);
-				
-		int a = 2;
-		int b = 3;
-		System.out.println("Hello Word!!! " + (a+b));
+		System.out.println(livros);				
+
 
 	}
 
